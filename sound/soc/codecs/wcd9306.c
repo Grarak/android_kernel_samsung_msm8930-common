11dd39fcf246d546f303e2ba83c527af1a8c2799
Willi Ye <williye97@gmail.com>
2014-05-21 16:26:52 +0200

    Revert "sound/soc/codecs/wcd9310.c: expose additional controls"
    This reverts commit 844312468c97852dfea1597bea212ae5845afe80.

--- sound/soc/codecs/wcd9306.c
+++ sound/soc/codecs/wcd9306.c
@@ -2939,10 +2939,7 @@ static int tapan_volatile(struct snd_soc_codec *ssc, unsigned int reg)
 }
 
 #define TAPAN_FORMATS (SNDRV_PCM_FMTBIT_S16_LE)
-#ifndef CONFIG_SOUND_CONTROL_HAX_GPL
-static
-#endif
-int tapan_write(struct snd_soc_codec *codec, unsigned int reg,
+static int tapan_write(struct snd_soc_codec *codec, unsigned int reg,
 	unsigned int value)
 {
 	int ret;
@@ -2961,14 +2958,7 @@ int tapan_write(struct snd_soc_codec *codec, unsigned int reg,
 
 	return wcd9xxx_reg_write(codec->control_data, reg, value);
 }
-#ifdef CONFIG_SOUND_CONTROL_HAX_GPL
-EXPORT_SYMBOL(tapan_write);
-#endif
-
-#ifndef CONFIG_SOUND_CONTROL_HAX_GPL
-static
-#endif
-unsigned int tapan_read(struct snd_soc_codec *codec,
+static unsigned int tapan_read(struct snd_soc_codec *codec,
 				unsigned int reg)
 {
 	unsigned int val;
@@ -2992,10 +2982,6 @@ unsigned int tapan_read(struct snd_soc_codec *codec,
 	val = wcd9xxx_reg_read(codec->control_data, reg);
 	return val;
 }
-
-#ifdef CONFIG_SOUND_CONTROL_HAX_GPL
-EXPORT_SYMBOL(tapan_read);
-#endif
 
 static int tapan_startup(struct snd_pcm_substream *substream,
 		struct snd_soc_dai *dai)
@@ -4515,8 +4501,6 @@ static struct wcd9xxx_reg_address tapan_reg_address = {
 #ifdef CONFIG_SOUND_CONTROL_HAX_GPL
 struct snd_kcontrol_new *gpl_faux_snd_controls_ptr =
     (struct snd_kcontrol_new *)tapan_snd_controls;
-struct snd_soc_codec *fauxsound_codec_ptr;
-EXPORT_SYMBOL(fauxsound_codec_ptr);
 #endif
 
 static int tapan_codec_probe(struct snd_soc_codec *codec)
@@ -4529,11 +4513,6 @@ static int tapan_codec_probe(struct snd_soc_codec *codec)
 	int ret = 0;
 	int i;
 	void *ptr = NULL;
-
-#ifdef CONFIG_SOUND_CONTROL_HAX_GPL
-    pr_info("tapan codec probe...\n");
-    fauxsound_codec_ptr = codec;
-#endif
 
 	codec->control_data = dev_get_drvdata(codec->dev->parent);
 	control = codec->control_data;
