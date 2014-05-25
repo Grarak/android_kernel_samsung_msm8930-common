53e9f4bcf7e1878ad728aef10e451322ce259b79
Willi Ye <williye97@gmail.com>
2014-05-25 20:41:30 +0200

    add CPU OC and CPU UC

--- arch/arm/mach-msm/board-melius.c
+++ arch/arm/mach-msm/board-melius.c
@@ -4081,11 +4081,7 @@ static struct msm_thermal_data msm_thermal_pdata = {
 	.sensor_id = 9,
 #ifdef CONFIG_INTELLI_THERMAL
 	.poll_ms = 250,
-#ifdef CONFIG_CPU_OVERCLOCK
 	.limit_temp_degC = 70,
-#else
-	.limit_temp_degC = 60,
-#endif
 	.temp_hysteresis_degC = 10,
         .freq_step = 2,
         .freq_control_mask = 0xf,
